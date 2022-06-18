d7ce4925c41d6d5f8e66ce9d03c1df62e4cd9a4e
khalil0258 <brahim13khalil@gmail.com>
2022-06-19 00:33:27 +0100

    messages section in process .did some of the friends section (right one)

---
+++ src/components/messages/Friends.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PostName from "../Posts/PostName";
+
+function Friends() {
+  return (
+    <div className="w-[370px] h-full border-r ">
+      <div className="text-center py-3 font-medium text-md border-b">
+        <h3>Khalil____hjz </h3>
+      </div>
+      <div className="overflow-y-scroll h-[460px]">
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+          <PostName/>
+      </div>
+    </div>
+  );
+}
+
+export default Friends;
--- src/pages/messages/Messages.js
+++ src/pages/messages/Messages.js
@@ -1,15 +1,20 @@
-
 import React from "react";
 import { Outlet, Route, Routes } from "react-router";
-
-import Message from "../../components/messages/Message";
+import HeaderHolder from "../../components/global/HeaderHolder";
+import { HeaderContainer } from "../../components/global/ContainerSignup";
+import Friends from "../../components/messages/Friends";
 
 function Messages() {
   return (
-    <div>
-      messages
-      <Outlet />
-    </div>
+    <HeaderHolder>
+      {/* the main section that combine the two routes  */}
+      <HeaderContainer>
+        <div className="w-full bg-white h-[510px] transform -translate-y-4 border  mx-auto rounded-lg">
+          <Friends />
+          <Outlet />
+        </div>
+      </HeaderContainer>
+    </HeaderHolder>
   );
 }
 
